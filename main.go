29ce2376b97a16eddf9d66cdaeda94b98e22cdca
Dwaipayan Biswas <biswastatay73@gmail.com>
2024-11-24 13:02:21 +0530

    first commit

---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+type URL struct {
+	ID           string    `json:"id"`
+	OriginalURL  string    `json:"original_url"`
+	ShortURL     string    `json:"short_url"`
+	CreationDate time.Time `json:"creation_date"`
+}
+
+var urlDB = make(map[string]URL)
+
+func generateShortURL(OriginalURL string) string {
+	hasher := md5.New()
+	hasher.Write([]byte(OriginalURL))
+	fmt.Println("hasher: ", hasher)
+	data := hasher.Sum(nil)
+	fmt.Println("hasher data: ", data)
+	hash := hex.EncodeToString(data)
+	fmt.Println("hash EncodeToString: ", hash)
+	fmt.Println("final String: ", hash[:8])
+	return hash[:8]
+
+}
+
+func createURL(originalURL string) string {
+	shortURL := generateShortURL(originalURL)
+	id := shortURL
+
+	urlDB[id] = URL{
+		ID:           id,
+		OriginalURL:  originalURL,
+		ShortURL:     shortURL,
+		CreationDate: time.Now(),
+	}
+	return shortURL
+}
+
+func getURL(id string) (URL, error) {
+	url, ok := urlDB[id]
+	if !ok {
+		return URL{}, errors.New("Url not found")
+	}
+	return url, nil
+}
+
+func main() {
+	fmt.Println("Dwaipayan Biswas")
+	OriginalURL := "https://github.com/dwaipayan7?tab=overview&from=2024-11-01&to=2024-11-08"
+	generateShortURL(OriginalURL)
+
+	fmt.Println("Starting server on port 3000")
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		fmt.Println("Error on starting server: ", err)
+	}
+
+}
